fdd29b38fe74d74c8608a19e41f9a7fbed7dcecb
Regina Salikhova <yanadera1873@gmail.com>
2025-07-03 11:59:42 +0400

    added jest, babel-jest

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
