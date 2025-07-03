62dc40db923fac888eb88e26f01d0b8bed935df3
Regina Salikhova <yanadera1873@gmail.com>
2025-07-03 11:40:37 +0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        title: "Document",
+        template: "./src/index.html"
+      }),
+      new MiniCSSExtractPlugin()
+    ]
+}
